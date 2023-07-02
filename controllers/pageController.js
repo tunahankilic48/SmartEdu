34e662a1582bcbfab0947fe21f3bfed5f8bfb2f2
Tunahan Kilic <tunahankilic942@gmail.com>
2023-07-02 14:23:41 +0300

    When the form was filled on the contact page, sending an e-mail was done.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer")
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userId)
     res.status(200).render('index', {
@@ -21,4 +23,44 @@ exports.getIndexPage = (req, res) => {
     res.status(200).render('login', {
       pageName: "login"
     });
+  }
+
+  exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+      pageName: "contact"
+    });
+  }
+
+  exports.sendEmail = async (req, res) => {
+    const outputMessage =`
+    <h1>Mail Details</h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: 'humanresourcehs8@gmail.com',
+        pass: 'letjphjlfbbzxdsd'
+      }
+    });
+
+      const info = await transporter.sendMail({
+        from: '"Smart Edu Contact Form" <humanresourcehs8@gmail.com>', // sender address
+        to: "kilictunahan942@gmail.com", // list of receivers
+        subject: "Smart Edu Contact Form New Message", // Subject line
+        html: outputMessage, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      res.status(200).redirect("contact")
   }
